e4d7c67b88744d85c1e78d942ab2d916c1033e07
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-03-04 16:22:01 +0100

    Implement simple checkerboard

--- hw2p2_checkerboard/quad/quad_fshader.glsl
+++ hw2p2_checkerboard/quad/quad_fshader.glsl
@@ -1,13 +1,18 @@
 #version 330
 
 in vec2 uv;
-
 out vec3 color;
-
 uniform sampler1D colormap;
 
 void main() {
-    color = vec3(1.0, 0.0, 0.0);
+    int x = int(floor(uv.x * 10));
+    int y = int(floor(uv.y * 10));
+    int col = 0;
+    if (mod(x, 2) == 0) {
+        col = mod(y, 2) == 0 ? 0 : 2;
+    } else {
+        col = mod(y, 2) == 0 ? 2 : 0;
+    }
+    //float value = sin(uv.x % 2); // Il faut utiliser sin de qqchose comme indiqué dans slides
+    color = texture(colormap, col).rgb;
 }
-
-
