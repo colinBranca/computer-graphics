9fd145ee4f7d7c8508006f2032ed9aada37717e4
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-03-06 14:01:07 +0100

    Fixed checkerboard

--- hw2p2_checkerboard/quad/quad_fshader.glsl
+++ hw2p2_checkerboard/quad/quad_fshader.glsl
@@ -5,14 +5,7 @@ out vec3 color;
 uniform sampler1D colormap;
 
 void main() {
-    int x = int(floor(uv.x * 10));
-    int y = int(floor(uv.y * 10));
-    int col = 0;
-    if (mod(x, 2) == 0) {
-        col = mod(y, 2) == 0 ? 0 : 2;
-    } else {
-        col = mod(y, 2) == 0 ? 2 : 0;
-    }
-    //float value = sin(uv.x % 2); // Il faut utiliser sin de qqchose comme indiqué dans slides
-    color = texture(colormap, col).rgb;
+    float sinx = sin(10 * 3.14 * uv.x);
+    float siny = sin(10 * 3.14 * uv.y);
+    color = texture(colormap, (siny * sinx + 1) / 2.0).rgb;
 }
