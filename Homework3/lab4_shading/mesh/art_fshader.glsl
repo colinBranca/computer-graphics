a18c71efa3520c835bb86b680885a61055ea66e4
colinBranca <colin.branca@epfl.ch>
2017-03-20 16:58:58 +0100

    Begin to fix artistic shader, DOES NOT WORK

--- Homework3/lab4_shading/mesh/art_fshader.glsl
+++ Homework3/lab4_shading/mesh/art_fshader.glsl
@@ -3,6 +3,8 @@
 
 out vec3 color;
 
+uniform vec3 La, Ld, Ls;
+uniform vec3 ka, kd, ks;
 uniform float alpha;
 uniform sampler2D tex2D;
 
@@ -15,11 +17,16 @@ void main() {
    //>>>>>>>>>> TODO >>>>>>>>>>>
     // TODO 3.2: Artistic shading.
     /// 3) compute specular term using the texture sampler tex.
-    vec3 r = normalize(2.0f * normal_mv * dot(normal_mv, light_dir) - light_dir);
 
-    float t1 = pow(dot(r, view_dir), alpha);
-    float t2 = dot(normal_mv, light_dir);
+    float t1 = dot(normal_mv, light_dir);
 
-    color = texture(tex2D, vec2(t2, t1)).rgb;
+    vec3 r = normalize(2.0f * normal_mv * t1 - light_dir);
+    float t2 = pow(dot(r, view_dir), alpha);
+
+    vec3 ambient = ka * La;
+    vec3 diffuse = kd * t1 * Ld;
+    vec3 specular = ks * texture(tex2D, vec2(t1, t2)).r * Ls;
+
+    color = ambient + diffuse + specular;
     //<<<<<<<<<< TODO <<<<<<<<<<<
 }
