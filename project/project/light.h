444497f2e38116f2984f255c788b62c99c7a9704
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-04-12 17:46:37 +0200

    Extract light from terrain as other objects might need it in the future

---
+++ project/project/light.h
@@ -0,0 +1,27 @@
+
+struct Light {
+        glm::vec3 La = glm::vec3(1.0f, 1.0f, 1.0f);
+        glm::vec3 Ld = glm::vec3(1.0f, 1.0f, 1.0f);
+        glm::vec3 Ls = glm::vec3(1.0f, 1.0f, 1.0f);
+
+        glm::vec3 light_pos = glm::vec3(0.0f, 0.0f, 2.0f);
+
+        // pass light properties to the shader
+        void Setup(GLuint program_id) {
+            glUseProgram(program_id);
+
+            // given in camera space
+            GLuint light_pos_id = glGetUniformLocation(program_id, "light_pos");
+
+            GLuint La_id = glGetUniformLocation(program_id, "La");
+            GLuint Ld_id = glGetUniformLocation(program_id, "Ld");
+            GLuint Ls_id = glGetUniformLocation(program_id, "Ls");
+
+            glUniform3fv(light_pos_id, ONE, glm::value_ptr(light_pos));
+            glUniform3fv(La_id, ONE, glm::value_ptr(La));
+            glUniform3fv(Ld_id, ONE, glm::value_ptr(Ld));
+            glUniform3fv(Ls_id, ONE, glm::value_ptr(Ls));
+        }
+};
+
+
--- project/project/terrain/terrain.h
+++ project/project/terrain/terrain.h
@@ -1,36 +1,12 @@
 #pragma once
 #include "icg_helper.h"
 #include <glm/gtc/type_ptr.hpp>
-
-struct Light {
-        glm::vec3 La = glm::vec3(1.0f, 1.0f, 1.0f);
-        glm::vec3 Ld = glm::vec3(1.0f, 1.0f, 1.0f);
-        glm::vec3 Ls = glm::vec3(1.0f, 1.0f, 1.0f);
-
-        glm::vec3 light_pos = glm::vec3(0.0f, 0.0f, 2.0f);
-
-        // pass light properties to the shader
-        void Setup(GLuint program_id) {
-            glUseProgram(program_id);
-
-            // given in camera space
-            GLuint light_pos_id = glGetUniformLocation(program_id, "light_pos");
-
-            GLuint La_id = glGetUniformLocation(program_id, "La");
-            GLuint Ld_id = glGetUniformLocation(program_id, "Ld");
-            GLuint Ls_id = glGetUniformLocation(program_id, "Ls");
-
-            glUniform3fv(light_pos_id, ONE, glm::value_ptr(light_pos));
-            glUniform3fv(La_id, ONE, glm::value_ptr(La));
-            glUniform3fv(Ld_id, ONE, glm::value_ptr(Ld));
-            glUniform3fv(Ls_id, ONE, glm::value_ptr(Ls));
-        }
-};
+#include "./../light.h"
 
 struct Material {
         glm::vec3 ka = glm::vec3(0.18f, 0.1f, 0.1f);
         glm::vec3 kd = glm::vec3(0.9f, 0.5f, 0.5f);
-        glm::vec3 ks = glm::vec3(0.8f, 0.8f, 0.8f);
+        //glm::vec3 ks = glm::vec3(0.8f, 0.8f, 0.8f);
         float alpha = 60.0f;
 
         // pass material properties to the shaders
@@ -39,17 +15,17 @@ struct Material {
 
             GLuint ka_id = glGetUniformLocation(program_id, "ka");
             GLuint kd_id = glGetUniformLocation(program_id, "kd");
-            GLuint ks_id = glGetUniformLocation(program_id, "ks");
+            //GLuint ks_id = glGetUniformLocation(program_id, "ks");
             GLuint alpha_id = glGetUniformLocation(program_id, "alpha");
 
             glUniform3fv(ka_id, ONE, glm::value_ptr(ka));
             glUniform3fv(kd_id, ONE, glm::value_ptr(kd));
-            glUniform3fv(ks_id, ONE, glm::value_ptr(ks));
+            //glUniform3fv(ks_id, ONE, glm::value_ptr(ks));
             glUniform1f(alpha_id, alpha);
         }
 };
 
-class Grid: public Light, public Material  {
+class Terrain: public Light, Material  {
 
     private:
         GLuint vertex_array_id_;                // vertex array object
@@ -59,19 +35,19 @@ class Grid: public Light, public Material  {
         GLuint height_texture_id_;              // texture ID
         GLuint colormap_texture_id_;            // texture ID
         GLuint num_indices_;                    // number of vertices to render
-        GLuint M_id_;                         // model, view, proj matrix ID
-        GLuint V_id_;                         // model, view, proj matrix ID
-        GLuint P_id_;                         // model, view, proj matrix ID
+        GLuint M_id_;                           // model matrix ID
+        GLuint V_id_;                           // proj matrix ID
+        GLuint P_id_;                           // view matrix ID
         int flattenCoord(int i, int j, int dim) {
                 return dim * i + j;
         }
 
     public:
-        void Init(size_t grid_dim, GLuint texture_id) {
+        void Init(size_t grid_dim, GLuint elevation_texture_id) {
             // compile the shaders.
-            program_id_ = icg_helper::LoadShaders("grid_vshader.glsl",
-                                                  "grid_fshader.glsl");
-            this->height_texture_id_ = texture_id;
+            program_id_ = icg_helper::LoadShaders("terrain_vshader.glsl",
+                                                  "terrain_fshader.glsl");
+            this->height_texture_id_ = elevation_texture_id;
             if(!program_id_) {
                 exit(EXIT_FAILURE);
             }
@@ -134,12 +110,14 @@ class Grid: public Light, public Material  {
                                       ZERO_STRIDE, ZERO_BUFFER_OFFSET);
             }
 
-           glBindTexture(GL_TEXTURE_2D, texture_id);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            // Received texture (elevation)
+            {
+                    glBindTexture(GL_TEXTURE_2D, elevation_texture_id);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-            GLuint h_tex_id = glGetUniformLocation(program_id_, "tex");
-            glUniform1i(h_tex_id, 0);
+                    glUniform1i(glGetUniformLocation(program_id_, "tex"), 0);
+            }
             // other uniforms
             M_id_ = glGetUniformLocation(program_id_, "M");
             V_id_ = glGetUniformLocation(program_id_, "V");
@@ -148,19 +126,17 @@ class Grid: public Light, public Material  {
             // create 1D texture (colormap)
             {
                 const int ColormapSize=4;
-                GLfloat colors[3*ColormapSize] = {/*dark braun*/ 184.0/255.0, 134.0/255.0, 11.0/255.0,
-                                                /*light braun*/ 218.0/255.0, 165.0/255.0, 32.0/255.0,
-                                               /*green*/ 34.0/255.0, 139.0/255.0, 34.0/255.0,
-                                               /*dark snow*/ 238.0/255.0, 233.0/255.0, 233.0/255.0 };
+                GLfloat colors[3*ColormapSize] = {/* dark brown  */ 184.0 / 255.0, 134.0 / 255.0, 11.0 / 255.0,
+                                                  /* light brown */ 218.0 / 255.0, 165.0 / 255.0, 32.0 / 255.0,
+                                                  /* green       */ 34.0  / 255.0, 139.0 / 255.0, 34.0 / 255.0,
+                                                  /* dark snow   */ 238.0 / 255.0, 233.0 / 255.0, 233  / 255.0 };
                 glGenTextures(1, &colormap_texture_id_);
                 glBindTexture(GL_TEXTURE_1D, colormap_texture_id_);
                 glTexImage1D(GL_TEXTURE_1D, 0, GL_RGB, ColormapSize, 0, GL_RGB, GL_FLOAT, colors);
                 glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
                 glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
                 glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                GLuint c_tex_id = glGetUniformLocation(program_id_, "colormap");
-                glUniform1i(c_tex_id, 1 /*GL_TEXTURE0*/);
-                // check_error_gl();
+                glUniform1i(glGetUniformLocation(program_id_, "colormap"), 1);
             }
 
             // to avoid the current object being polluted
