6ce2641726ae02fa6b4902cb72c61c127ec340c8
colinBranca <colin.branca@epfl.ch>
2017-05-09 10:35:38 +0200

    Made smooth waves

--- project/project/water/water_vshader.glsl
+++ project/project/water/water_vshader.glsl
@@ -14,7 +14,7 @@ void main() {
     uv = getTexCoord(position);
     vec2 uvWave = uv;
     uvWave.x = 0.1f*time+uvWave.x;
-    float wave = texture(wave_tex, uvWave).x / 2.0f ;
+    float wave = texture(wave_tex, uvWave).x / 10.0f ;
 
     vec3 pos_3d = vec3(position.x, water_height + wave, position.y);
     gl_Position = MVP * vec4(pos_3d, 1.0);
