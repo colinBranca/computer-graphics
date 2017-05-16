83b9ef8aacd9dc4c0f400ccfa73b63f2313975fa
colinBranca <colin.branca@epfl.ch>
2017-05-16 16:46:34 +0200

    Made smoother waves

--- project/project/water/water_vshader.glsl
+++ project/project/water/water_vshader.glsl
@@ -14,35 +14,16 @@ uniform float cell_size;
 uniform float water_height;
 uniform float time;
 
-const float weight = 1.0f;
-const float amplitude = 0.1f;
-const float phase = 1.0f;
 vec3 wave_position = vec3(0.0f, water_height, 0.0f);
 
-// float computeHeight(float x, float z, vec2 direction) {
-//     return amplitude * sin(dot(direction, vec2(x, mod(z, 2.0f))) * weight + time * phase);
-// }
-//
-// float computeHeight(float x, float z, vec2 direction) {
-//     return amplitude * sin(x * weight + time * phase);
-// }
-
 float computeHeight(float x, float z) {
-  x += 0.01f*time;
-  return texture(tex_wave, vec2(x,z)).x /10.0f;
+  x += 0.005f*time;
+  return texture(tex_wave, vec2(x,z)).x /50.0f;
 }
 
 vec2 getTexCoord(vec2 position) {
     return (position + vec2(5.0f, 5.0f)) * 0.1f;
 }
-
-// vec3 computeNormal(float x, float z, vec2 direction) {
-//     float height_east = computeHeight(x + cell_size, z, direction);
-//     float height_west = computeHeight(x - cell_size, z, direction);
-//     float height_north = computeHeight(x, z - cell_size, direction);
-//     float height_south = computeHeight(x, z + cell_size, direction);
-//     return normalize(vec3(height_west - height_east, 1.0f, height_south - height_north));
-// }
 
 vec3 computeNormal(float x, float z) {
     float height_east = computeHeight(x + cell_size, z);
@@ -54,7 +35,6 @@ vec3 computeNormal(float x, float z) {
 
 void main() {
     vec2 uv = getTexCoord(position.xz);
-    vec2 direction = (wave_position - position).xz;
     vec3 adjusted_position =
         vec3(position.x, water_height + computeHeight(uv.x, uv.y), position.z);
 
